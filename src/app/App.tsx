02f8b5a049f97d55986c29c97fbef73d448df092
mirzayv <mirzayv919@gmail.com>
2026-06-27 02:57:46 +0900

    fix: develop burak screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,8 +5,10 @@ import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Link, Route, Switch } from "react-router-dom";
-import { About } from "./screens/About";
-import { Users } from "./screens/Users";
+import { HomePage } from "./screens/homePage/index";
+import { UserPage } from "./screens/userPage/index";
+import { OrdersPage } from "./screens/ordersPage/index";
+import { ProductsPage } from "./screens/productsPage/index";
 
 function App() {
   return (
@@ -14,35 +16,39 @@ function App() {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">HomePage</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/products">ProductsPage</Link>
           </li>
           <li>
-            <Link to="/users">Users</Link>
+            <Link to="/orders">OrdersPage</Link>
+          </li>
+          <li>
+            <Link to="/member-page">UserPage</Link>
           </li>
         </ul>
       </nav>
 
       <Switch>
-        <Route path="/about">
-          <About />
+        <Route path="/products">
+          <ProductsPage />
         </Route>
 
-        <Route path="/users">
-          <Users />
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+
+        <Route path="/member-page">
+          <UserPage />
         </Route>
 
         <Route path="/">
-          <Home />
+          <HomePage />
         </Route>
       </Switch>
     </div>
   );
-}
-function Home() {
-  return <Container>Home</Container>;
 }
 
 export default App;
