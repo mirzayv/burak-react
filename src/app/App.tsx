d1a2ceefe78dc532686d5fb29e250ebc4cfd19fe
mirzayv <mirzayv919@gmail.com>
2026-06-27 03:24:13 +0900

    fix: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,31 +4,20 @@ import "../css/app.css";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
 import { HomePage } from "./screens/homePage/index";
 import { UserPage } from "./screens/userPage/index";
 import { OrdersPage } from "./screens/ordersPage/index";
 import { ProductsPage } from "./screens/productsPage/index";
-
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { Footer } from "./components/footer";
 function App() {
+  const location = useLocation();
+
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">HomePage</Link>
-          </li>
-          <li>
-            <Link to="/products">ProductsPage</Link>
-          </li>
-          <li>
-            <Link to="/orders">OrdersPage</Link>
-          </li>
-          <li>
-            <Link to="/member-page">UserPage</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
 
       <Switch>
         <Route path="/products">
@@ -47,7 +36,8 @@ function App() {
           <HomePage />
         </Route>
       </Switch>
-    </div>
+      <Footer />
+    </>
   );
 }
 
