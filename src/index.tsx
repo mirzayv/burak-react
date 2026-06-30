792ed6e28b17e28d35fbb712edaccdbb771b389e
mirzayv <mirzayv919@gmail.com>
2026-06-30 15:57:45 +0900

    fix: modifiy root index render logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,31 @@
+// @ts-nocheck
 import React from "react";
+import {createRoot} from "react-dom/client"; 
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
-import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "./app/MaterialTheme";
-import { BrowserRouter as Router } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./app/material/MaterialTheme";
 import "./css/index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {/* @ts-ignore */}
         <Router>
           <App />
         </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root"),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
